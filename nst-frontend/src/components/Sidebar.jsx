10b6ee80aa90d9e7be7cc061e3eea05a7fef5a8f
Isidora Radisavljevic <isidoraradisavljevic22@gmail.com>
2025-08-25 00:19:55 +0200

    Dodavanje opcija podmenija sidebar-a

--- nst-frontend/src/components/Sidebar.jsx
+++ nst-frontend/src/components/Sidebar.jsx
@@ -8,33 +8,70 @@ function Sidebar({setIsLoggedIn}) {
     const navigator=useNavigate();
     const location=useLocation();
     const [isOpen, setIsOpen] = useState(false);
+    const[subnav, setSubnav]=useState({});
+
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     };
+
+    const toggleSubnav = (index) => {
+        setSubnav(prev => ({ ...prev, [index]: !prev[index] }));
+    };
+
     useEffect(() => {
         setIsOpen(false);
     }, [location]);
 
   return (
     <div>
-        <div className="hamburger" onClick={toggleSidebar}>
-            <FaBars />
+      <div className="hamburger" onClick={toggleSidebar}>
+        <FaBars />
+      </div>
+
+      {isOpen && (
+        <div className="Sidebar">
+          <ul className="SidebarList">
+            {SidebarData.map((val, key) => (
+              <>
+                <li
+                  key={key}
+                  className="row"
+                  id={window.location.pathname === val.link ? 'active' : ''}
+                  onClick={() => {
+                    if (val.subNav) {
+                      toggleSubnav(key);
+                    } else {
+                      navigator(val.link);
+                    }
+                  }}
+                >
+                <div className="row-content">
+                  <div id="icon">{val.icon}</div>
+                  <div id="title">{val.title}</div>
+                </div>
+                    {val.subNav && (subnav[key] ? val.iconOpened : val.iconClosed)}
+                
+               
+                </li>
+
+                {val.subNav && subnav[key] && (
+                  <ul className="SubMenu">
+                    {val.subNav.map((sub, subIndex) => (
+                      <li
+                        key={subIndex}
+                        className="subRow"
+                        onClick={() => navigator(sub.link)}
+                      >
+                        <div id="title">{sub.title}</div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </>
+            ))}
+          </ul>
         </div>
-        {isOpen && <div className='Sidebar'>
-         <ul className='SidebarList'>
-            {SidebarData.map((val, key)=>{
-                return(
-                    <li key={key}
-                    className='row'
-                    id={window.location.pathname ==val.link ? 'active': ''}
-                    onClick={()=>{navigator(val.link);}}>
-                        <div id="icon">{val.icon}</div>
-                        <div id="title">{val.title}</div>
-                    </li>
-                )
-            })}
-        </ul> </div>}
-        
+      )}
     </div>
   )
 }
--- nst-frontend/src/components/SidebarData.jsx
+++ nst-frontend/src/components/SidebarData.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import HomeIcon from '@mui/icons-material/Home';
+import CoffeeIcon from '@mui/icons-material/Coffee';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export const SidebarData = [
     {
@@ -8,8 +11,66 @@ export const SidebarData = [
         link:'/'
     },
     {
-        title:'Other',
-        icon: <HomeIcon></HomeIcon>,
-        link:'/dobavljaci'
+        title:'Proizvodi',
+        icon: <CoffeeIcon></CoffeeIcon>,
+        link:'/proizvodi',
+        iconClosed: <KeyboardArrowDownIcon></KeyboardArrowDownIcon>,
+        iconOpened: <KeyboardArrowUpIcon></KeyboardArrowUpIcon>,
+        subNav:[
+            {
+                title:'Prikaži sve proizvode',
+                link:'/proizvodi'
+            },
+            {
+                title:'Pronađi proizvod',
+                link:'/'
+            },
+            {
+                title:'Dodaj proizvod',
+                link:'/dodaj-proizvod'
+            }
+        ]
+    },
+    {
+        title:'Dobavljači',
+        icon: <CoffeeIcon></CoffeeIcon>,
+        link:'/dobavljaci',
+        iconClosed: <KeyboardArrowDownIcon></KeyboardArrowDownIcon>,
+        iconOpened: <KeyboardArrowUpIcon></KeyboardArrowUpIcon>,
+        subNav:[
+            {
+                title:'Prikaži sve dobavljače',
+                link:'/dobavljaci'
+            },
+            {
+                title:'Pronađi dobavljača',
+                link:'/'
+            },
+            {
+                title:'Dodaj dobavljača',
+                link:'/dodaj-dobavljaca'
+            }
+        ]
+    },
+    {
+        title:'Narudžbenice',
+        icon: <CoffeeIcon></CoffeeIcon>,
+        link:'/',
+        iconClosed: <KeyboardArrowDownIcon></KeyboardArrowDownIcon>,
+        iconOpened: <KeyboardArrowUpIcon></KeyboardArrowUpIcon>,
+        subNav:[
+            {
+                title:'Prikaži sve narudžbenice',
+                link:'/'
+            },
+            {
+                title:'Pronađi narudžbenicu',
+                link:'/prikazi-narudzbenicu'
+            },
+            {
+                title:'Dodaj narudžbenicu',
+                link:'/dodaj-narudzbenicu'
+            }
+        ]
     }
 ];
